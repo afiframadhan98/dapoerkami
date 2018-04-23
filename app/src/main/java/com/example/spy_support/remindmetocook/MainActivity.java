7ef0d877ff88f272d049b851ee10cf9b6bc9d569
sannyph <sannyh22@yahoo.com>
2018-04-23 12:09:50 +0700

    layout reminder to cook #5

--- app/src/main/java/com/example/spy_support/remindmetocook/MainActivity.java
+++ app/src/main/java/com/example/spy_support/remindmetocook/MainActivity.java
@@ -1,48 +1,111 @@
 package com.example.spy_support.remindmetocook;
 
+import android.app.ProgressDialog;
+import android.content.ContentUris;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.app.LoaderManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private FloatingActionButton mAddReminderButton;
+    private Toolbar mToolbar;
+    AlarmCursorAdapter alarmCursorAdapter;
+    AlarmReminderDbHelper alarmReminderDbHelper = new AlarmRemniderDbHelper (this);
+    Listview reminderListView;
+    ProgressDialog prgDialog;
+
+    private static final int VEHICLE_LOADER = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
 
-    public void showDatePickerDialog(View v) {
-        DialogFragment newFragment = new PilihTanggal();
-        newFragment.show(getSupportFragmentManager(), getString(R.string.date_picker));
-    }
+        mToolbar = (Toolbar) findViewById( R.id.toolbar );
+        setSupportActionBar( mToolbar );
+        mToolbar.setTitle( "AlarmReminder" );
 
-    public void showTimePickerDialog(View view) {
-        DialogFragment newFragment = new PilihWaktu();
-        newFragment.show(getSupportFragmentManager(), getString(R.string.time_picker));
-    }
+        reminderListView = (ListView) findViewById( R.id.List );
+        View emptyview = findViewById( R.id.empty_view );
+        reminderListView.setEmptyView( emptyview );
 
-    public void processDatePickerResult(int year, int month, int day) {
-        // The month integer returned by the date picker starts counting at 0
-        // for January, so you need to add 1 to show months starting at 1.
-        String month_string = Integer.toString(month + 1);
-        String day_string = Integer.toString(day);
-        String year_string = Integer.toString(year);
-        // Assign the concatenated strings to dateMessage.
-        String dateMessage = (month_string + "/" + day_string + "/" + year_string);
-        Toast.makeText(this, getString(R.string.tanggal) + dateMessage, Toast.LENGTH_SHORT).show();
-    }
+        mCursorAdapter = new AlarmCursorAdapter( this, null );
+        reminderListView.setAdapter( mCursorAdapter );
 
-    public void processTimePickerResult(int hourOfDay, int minute) {
-        // Convert time elements into strings.
-        String hour_string = Integer.toString(hourOfDay);
-        String minute_string = Integer.toString(minute);
-        // Assign the concatenated strings to timeMessage.
-        String timeMessage = (hour_string + ":" + minute_string);
-        Toast.makeText(this, getString(R.string.waktu) + timeMessage, Toast.LENGTH_SHORT).show();
-    }
+        reminderListView.setOnItemClickListener( new AdapterView.OnItemClickListener() {
 
+            @Override
+            public void onItemClick(AdapterView<?> adapterview, View view, int position, long id) {
+
+                Intent intent = new intent( MainActivity.this, AddReminderActivity.class );
+
+                Uri currentVehicleUri = ContentUris.withAppendedId( AlarmReminderContract.AlarmReminderEntry.CONTENT_URI, id );
+
+                intent.setData( currentVehicleUri );
+
+                startActivity( intent );
+            }
+        } );
+
+        mAddReminderButton = (FloatingActionButton) findViewById( R.id.fab );
+
+        mAddReminderButton.setOnClickListener( (v) {
+                Intent intent = new Intent( v.getContext(), AddReminderActivity.class );
+        startActivity( intent );
+        });
+
+        getLoaderManager().initLoader( VEHICLE_LOADER, null, this );
+    }
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+    String[] projection = {
+            AlarmReminderContract.AlarmReminderEntry._ID,
+            AlarmReminderContract.AlarmReminderEntry.KEY_TITLE,
+            AlarmReminderContract.AlarmReminderEntry.KEY_DATE,
+            AlarmReminderContract.AlarmReminderEntry.KEY_TIME,
+            AlarmReminderContract.AlarmReminderEntry.KEY_REPEAT,
+            AlarmReminderContract.AlarmReminderEntry.KEY_REPEAT_NO,
+            AlarmReminderContract.AlarmReminderEntry.KEY_REPEAT_TYPE,
+            AlarmReminderContract.AlarmReminderEntry.KEY_ACTIVE
+        );
+
+        return new CursorLoader (this,
+                AlarmReminderContract.AlarmReminderEntry.CONTENT_URI,
+                projection,
+                null,
+                null,
+                null);
+        }
+        @Override
+        public void onLoadFinished(Loader<Cursor> loader, Cursor cursor){
+            mCursorAdapter.swapCursor(cursor);
+        }
+
+        @Override
+        public void onLoaderReset(Loader<Cursor> loader) {
+            mCursorAdapter.swapCursor(null);
+    }
 }
+
+    private class Listview {
+    }
+
+    private class AlarmReminderDbHelper {
+    }
+
+    private class AlarmRemniderDbHelper extends AlarmReminderDbHelper {
+        public AlarmRemniderDbHelper(MainActivity mainActivity) {
+            super();
+        }
+    }
